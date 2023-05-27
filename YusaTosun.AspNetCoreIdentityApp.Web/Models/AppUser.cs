9c9fc3c8a3c68b36938836801aed355e816a3367
Yusa Tosun <yusatosun.yt@gmail.com>
2023-05-27 14:28:08 +0300

    Add project files.

---
+++ YusaTosun.AspNetCoreIdentityApp.Web/Models/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace YusaTosun.AspNetCoreIdentityApp.Web.Models
+{
+    public class AppUser:IdentityUser
+    {
+        public string City { get; set; }
+    }
+}
