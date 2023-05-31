6a3493ca620b1cac44f20149e4e91a790f7b7110
Yusa Tosun <yusatosun.yt@gmail.com>
2023-06-01 00:02:58 +0300

    SignUp Page

---
+++ YusaTosun.AspNetCoreIdentityApp.Web/ViewModels/SignUpWiewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YusaTosun.AspNetCoreIdentityApp.Web.ViewModels
+{
+    public class SignUpWiewModel
+    {
+        public SignUpWiewModel(string userName, string email, string phone, string password)
+        {
+            UserName = userName;
+            Email = email;
+            Phone = phone;
+            Password = password;
+        }
+
+        [Display(Name ="Kullanıcı Adı :")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Email :")]
+        public string Email { get; set; }
+
+        [Display(Name = "Telefon :")]
+        public string Phone { get; set; }
+
+        [Display(Name = "Şifre :")]
+        public string Password { get; set; }
+
+         [Display(Name = "Şifre Tekrar :")]
+        public string PasswordConfirm { get; set; }
+
+
+    }
+}
