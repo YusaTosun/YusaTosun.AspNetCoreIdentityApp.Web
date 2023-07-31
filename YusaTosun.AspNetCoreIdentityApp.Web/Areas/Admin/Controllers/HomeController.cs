e006a29c9ec7749117c832f318c8badf224b4cba
Yusa Tosun <yusatosun.yt@gmail.com>
2023-07-31 22:06:52 +0300

    Admin area added

---
+++ YusaTosun.AspNetCoreIdentityApp.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YusaTosun.AspNetCoreIdentityApp.Web.Areas.Admin.Models;
+using YusaTosun.AspNetCoreIdentityApp.Web.Models;
+
+namespace YusaTosun.AspNetCoreIdentityApp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> UserList()
+        {
+            var userList = await _userManager.Users.ToListAsync();
+
+            var userViewModelList = userList.Select(x=>new UserViewModel()
+            {
+               Id = x.Id,
+               Email= x.Email,
+               Name=x.UserName
+            }).ToList();
+            return View(userList);
+        }
+    }
+}
--- YusaTosun.AspNetCoreIdentityApp.Web/Program.cs
+++ YusaTosun.AspNetCoreIdentityApp.Web/Program.cs
@@ -28,8 +28,15 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=SignUp}/{id?}");
+
+
 
 app.Run();
