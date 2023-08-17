2ecc08b58afbcc8762dcc20df2b63918d2f1b2be
Yusa Tosun <yusatosun.yt@gmail.com>
2023-08-17 18:32:24 +0300

    hataların Türkçeleştirilmesi

---
+++ YusaTosun.AspNetCoreIdentityApp.Web/Localizations/LocalizationIdentityErrorDescriber.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace YusaTosun.AspNetCoreIdentityApp.Web.Localizations
+{
+    public class LocalizationIdentityErrorDescriber : IdentityErrorDescriber
+    {
+        public override IdentityError DuplicateUserName(string userName)
+        {
+            return new IdentityError() { Code = "DuplidateUserName", Description = $"{userName} başka bir kullanıcı tarafından alınmıştır" };
+        }
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return new IdentityError() { Code = "DuplidateEmail", Description = $"{email}  başka bir kullanıcı tarafından alınmıştır" };
+        }
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError() { Code = "PasswordTooShort", Description = $"Şifre en az 6 karakterli olmalı" };
+        }
+    }
+}
